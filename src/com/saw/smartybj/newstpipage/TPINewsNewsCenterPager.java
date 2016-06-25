888d86661f2263433f1977eec0b79e10596f4f53
slhy <824076046@qq.com>
2016-06-25 23:54:38 +0800

    新闻中心listView添加数据

--- src/com/saw/smartybj/newstpipage/TPINewsNewsCenterPager.java
+++ src/com/saw/smartybj/newstpipage/TPINewsNewsCenterPager.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.graphics.Bitmap.Config;
+import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -29,6 +34,7 @@ import com.saw.smartybj.MainActivity;
 import com.saw.smartybj.R;
 import com.saw.smartybj.domain.NewsCenterData.NewsData.ViewTagData;
 import com.saw.smartybj.domain.TPINewsData;
+import com.saw.smartybj.domain.TPINewsData.TPINewsData_Data.TPINewsData_Data_ListNewsData;
 import com.saw.smartybj.domain.TPINewsData.TPINewsData_Data.TPINewsData_Data_LunboData;
 import com.saw.smartybj.utils.DensityUtil;
 import com.saw.smartybj.utils.MyConstants;
@@ -50,6 +56,9 @@ public class TPINewsNewsCenterPager {
 	
 	@ViewInject(R.id.ll_tpi_news_pic_points)
 	private LinearLayout ll_points;//轮播图的每张图片对应的点
+	@ViewInject(R.id.lv_tpi_news_listnews)
+	private ListView lv_listnews;//listview新闻列表
+	
 	
 	//数据
 	private MainActivity mainActivity;
@@ -69,9 +78,20 @@ public class TPINewsNewsCenterPager {
 
 	private int picSelectIndex;
 
+	private Handler handler;
+
+	private LunBoTask lunboTask;
+	//新闻列表的数据
+	private List<TPINewsData_Data_ListNewsData> listNews = new ArrayList<TPINewsData.TPINewsData_Data.TPINewsData_Data_ListNewsData>();
+
+	private ListNewsAdapter listNewsAdapter;
+
 	public TPINewsNewsCenterPager(MainActivity mainActivity, ViewTagData viewTagData) {
 		this.mainActivity = mainActivity;
 		this.viewTagData = viewTagData;
+		gson = new Gson();
+		handler = new Handler();
+		lunboTask = new LunBoTask();
 		//xutils bitmap组件
 		bitmapUtils = new BitmapUtils(mainActivity);
 		bitmapUtils.configDefaultBitmapConfig(Config.ARGB_4444);//一个字节
@@ -111,6 +131,11 @@ public class TPINewsNewsCenterPager {
 		//给轮播图
 		vp_lunbo.setAdapter(lunboAdapter);
 		
+		//新闻列表适配器
+		listNewsAdapter = new ListNewsAdapter();
+		lv_listnews.setAdapter(listNewsAdapter);
+		
+		
 		//轮播图的数据
 		//新闻列表的数据
 		//从本地获取数据
@@ -127,7 +152,6 @@ public class TPINewsNewsCenterPager {
 	}
 	
 	private TPINewsData parseJson(String jsonData) {
-		gson = new Gson();
 		TPINewsData tpiNewsData = gson.fromJson(jsonData, TPINewsData.class);
 		return tpiNewsData;
 	}
@@ -141,8 +165,68 @@ public class TPINewsNewsCenterPager {
 		picSelectIndex = 0;
 		//3.设置图片描述和点的选中选过
 		setPicAndPointSelect(picSelectIndex);
+		//4.开始轮播图
+		lunboTask.startLunbo();
+		//5.新闻列表的数据
+		setListViewNews(newsData);
 		
 		
+	}
+	/**
+	 * 设置新闻列表的数据
+	 * @param newsData
+	 */
+	private void setListViewNews(TPINewsData newsData) {
+		listNews = newsData.data.news;
+		//更新界面
+		listNewsAdapter.notifyDataSetChanged();
+		
+	}
+
+	/**
+	 * 轮播图的处理
+	 */
+	private void lunboProcess() {
+		if (handler == null) {
+		
+			handler = new Handler();
+		}
+		//清空掉原来所有的任务
+		handler.removeCallbacksAndMessages(null);
+		
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				//任务
+				//控制轮播图的显示
+				vp_lunbo.setCurrentItem((vp_lunbo.getCurrentItem() + 1) % vp_lunbo.getAdapter().getCount());
+				handler.postDelayed(this, 2000);
+			}
+		}, 2000);
+	}
+	/**
+	 * 轮播图的处理
+	 * @author Administrator
+	 * @创建时间 2016-6-25 下午10:25:43
+	 * @描述 TODO
+	 */
+	private class LunBoTask extends Handler implements Runnable {
+		public void stopLunbo() {
+			//清空掉原来所有的任务
+			handler.removeCallbacksAndMessages(null);
+		}
+		public void startLunbo() {
+			stopLunbo();
+			handler.postDelayed(this, 2000);
+		}
+		@Override
+		public void run() {
+			//控制轮播图的显示
+			vp_lunbo.setCurrentItem((vp_lunbo.getCurrentItem() + 1 % vp_lunbo.getAdapter().getCount()));
+			//handler.postDelayed(this, 2000);
+			startLunbo();
+		}
 	}
 	/**
 	 * 设置图片描述和点的选中选过
@@ -185,6 +269,60 @@ public class TPINewsNewsCenterPager {
 		lunboAdapter.notifyDataSetChanged();
 		
 	}
+	private class ListNewsAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return listNews.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder = null;
+			if (holder == null) {
+				convertView = View.inflate(mainActivity, R.layout.tpi_news_listview_item, null);
+				holder = new ViewHolder();
+				holder.iv_icon = (ImageView) convertView.findViewById(R.id.iv_tpi_news_listview_item_icon);
+				holder.iv_newspic = (ImageView) convertView.findViewById(R.id.iv_tpi_news_listview_item_pic);
+				holder.tv_time = (TextView) convertView.findViewById(R.id.tv_tpi_news_listview_item_time);
+				holder.tv_title = (TextView) convertView.findViewById(R.id.tv_tpi_news_listview_item_title);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			//设置数据
+			TPINewsData_Data_ListNewsData tpiNewsData_Data_ListNewsData = listNews.get(position);
+			//设置标题
+			holder.tv_title.setText(tpiNewsData_Data_ListNewsData.title);
+			//设置时间
+			holder.tv_time.setText(tpiNewsData_Data_ListNewsData.pubdate);
+			//设置图片
+			bitmapUtils.display(holder.iv_newspic, MyConstants.REQUEST_HOST+tpiNewsData_Data_ListNewsData.listimage);
+			
+			return convertView;
+		}
+		
+	}
+	
+	private class ViewHolder {
+		ImageView iv_newspic;
+		TextView tv_title;
+		TextView tv_time;
+		ImageView iv_icon;
+	}
 	/**
 	 * @author Administrator
 	 * @创建时间 2016-6-19 下午10:36:16
@@ -225,6 +363,51 @@ public class TPINewsNewsCenterPager {
 			//把url的图片给iv_lunbo_pic
 			//异步加载图片，并且显示到组件中
 			bitmapUtils.display(iv_lunbo_pic, topimageUrl);
+			//给图片 添加触摸事件
+			iv_lunbo_pic.setOnTouchListener(new OnTouchListener() {
+				
+				private float downX;
+				private float downY;
+				private float upX;
+				private float upY;
+				private long downTime;
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN://按下停止轮播
+						downX = event.getX();
+						downY = event.getY();
+						downTime = System.currentTimeMillis();
+						lunboTask.stopLunbo();
+						break;
+					case MotionEvent.ACTION_CANCEL://事件取消开始轮播图
+						lunboTask.startLunbo();
+						break;
+					case MotionEvent.ACTION_UP://松开开始轮播
+						upX = event.getX();
+						upY = event.getY();
+						if (upX == downX && upY == downY) {
+							long upTime = System.currentTimeMillis();
+							if  (upTime - downTime < 500) {//单击事件
+								lunboPicClick("被单击了");
+							}
+						}
+						lunboTask.startLunbo();
+						break;
+					default:
+						break;
+					}
+					return true;
+				}
+				/**
+				 * 处理图片的单击事件
+				 */
+				private void lunboPicClick(Object data) {
+					System.out.println(data);
+				}
+			});
+			
 			container.addView(iv_lunbo_pic);
 			return iv_lunbo_pic;
 		}
@@ -258,8 +441,12 @@ public class TPINewsNewsCenterPager {
 	private void initView() {
 		//获取页签对应的根布局
 		root = View.inflate(mainActivity, R.layout.tpi_news_content, null);
-		
 		ViewUtils.inject(this, root);
+		
+		View lunBoPic = View.inflate(mainActivity, R.layout.tpi_news_lunbopic, null);
+		ViewUtils.inject(this, lunBoPic);
+		//把轮播图加到ListView中
+		lv_listnews.addHeaderView(lunBoPic);
 	}
 	public View getRoot() {
 		return root;
