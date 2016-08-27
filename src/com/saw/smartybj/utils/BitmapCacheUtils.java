38f10bda62e3539025a18f6d6091c49037f82059
slhy <824076046@qq.com>
2016-08-27 10:23:42 +0800

    修复网络加载图片错位的问题

--- src/com/saw/smartybj/utils/BitmapCacheUtils.java
+++ src/com/saw/smartybj/utils/BitmapCacheUtils.java
@@ -8,6 +8,10 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.saw.smartybj.MainActivity;
 
@@ -25,6 +29,7 @@ import android.widget.ImageView;
 public class BitmapCacheUtils {
 	//动态获取jvm内存
 	private int maxSize = (int) (Runtime.getRuntime().freeMemory() / 3);
+	//图片的缓存容器
 	private LruCache<String, Bitmap> memCache = new LruCache<String, Bitmap>(maxSize) {
 
 		@Override
@@ -34,18 +39,25 @@ public class BitmapCacheUtils {
 		}
 		
 	};
+	//保留最后一次访问url的信息
+	private Map<ImageView, String> urlImageViewDatas = new HashMap<ImageView, String>();
 	private File cacheDir;
 	private MainActivity mainActivity;
+	private ExecutorService threadPool;
 	public BitmapCacheUtils(MainActivity mainActivity) {
 		this.mainActivity = mainActivity;
 		//获取当前app的cache目录
 		cacheDir = mainActivity.getCacheDir();
+		//线程池
+		threadPool = Executors.newFixedThreadPool(6);//这里的数字和你手机屏幕一屏显示的图片个数（listview是3个，gridview是6个）
+		
+		
 	}
 	public void display(ImageView iv,String ivUrl) {
 		//1.先从内存中取
 		Bitmap bitmap = memCache.get(ivUrl);
 		if (bitmap != null) {
-			//System.out.println("从内存获取数据");
+			System.out.println("从内存获取数据");
 			//缓存中有图片
 			iv.setImageBitmap(bitmap);
 			return;
@@ -54,18 +66,20 @@ public class BitmapCacheUtils {
 		//2.再从本地文件取,app(/data/data/包名/cache)
 		bitmap = getCacheFile(ivUrl);
 		if (bitmap != null) {
-			//System.out.println("从文件获取数据");
+			System.out.println("从文件获取数据");
 			//本地文件中有图片
 			iv.setImageBitmap(bitmap);
 			return;
 		}
 		//3.从网络取
+		urlImageViewDatas.put(iv, ivUrl);//保留最后一次访问的url
 		getBitmapFromNet(iv,ivUrl);
 	}
 	private void getBitmapFromNet(ImageView iv, String ivUrl) {
 		//访问网络
-		new Thread(new DownloadUrl(iv, ivUrl)).start();
-		
+		//new Thread(new DownloadUrl(iv, ivUrl)).start();
+		//使用线程池
+		threadPool.submit(new DownloadUrl(iv, ivUrl));
 		
 	}
 	private class DownloadUrl implements Runnable {
@@ -97,9 +111,15 @@ public class BitmapCacheUtils {
 						
 						@Override
 						public void run() {
-							//System.out.println("从网络获取数据");
+							System.out.println("从网络获取数据");
 							//显示图片
-							iv.setImageBitmap(bitmap);
+							//判断URL是不是最新的
+							//是最新的 是自己的数据
+							if (ivUrl.equals(urlImageViewDatas.get(iv))) {
+								//自己的数据  url一致
+								iv.setImageBitmap(bitmap);
+							}
+							
 						}
 					});
 					
